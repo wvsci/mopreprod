f543aa47dfd2bc255908d248f9f9917d59b00d31
wvsci <wvsci@mail>
2022-04-17 20:29:36 +0300

    done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    $('.header-slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/up.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/down.svg" alt=""></button>',
+        vertical: true
+    });
+
+
+
+});
